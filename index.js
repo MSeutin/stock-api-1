9b3b4714a3115685d6e6fea9e74d607a5201fabc
Michael Seutin <45352998+MSeutin@users.noreply.github.com>
2022-06-19 21:52:42 -0700

    add more

--- index.js
+++ index.js
@@ -10,27 +10,30 @@ let startPrice = document.querySelector(".startPrice");
 let two = "FXIA00Z3WNN45IAZ";
 let endPrice = document.querySelector(".endPrice");
 let profitLoss = document.querySelector(".profitLoss");
+let modal = document.querySelector(".modal");
+let closeBtn = document.querySelector(".close-btn");
 
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  tickerResult.textContent = ticker.value;
-  let url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${ticker.value}&outputsize=full&${one}${two}`;
+  tickerResult.textContent = ticker.value.toUpperCase();
+  let url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${ticker.value.toUpperCase()}&outputsize=full&${one}${two}`;
+  console.log(`startDate: ${start.value}`)
   getData(url, start.value, end.value);
 });
 
 async function getData(url, startDate, endDate) {
-  const response = await fetch(url);
+
+  try {
+      const response = await fetch(url);
   let data = await response.json();
-  while (!data["Time Series (Daily)"][startDate]) {
-    console.log(`INVALID START DATE: ${startDate}`);
-    break;
-  }
   startDateResult.textContent = startDate;
   endDateResult.textContent = endDate;
+  // console.log(data["Time Series (Daily)"]);
   startPrice.textContent = Number(
     data["Time Series (Daily)"][startDate]["4. close"]
   ).toFixed(2);
+  console.log(startPrice.textContent);
   endPrice.textContent = Number(
     data["Time Series (Daily)"][endDate]["4. close"]
   ).toFixed(2);
@@ -39,4 +42,13 @@ async function getData(url, startDate, endDate) {
   let change = e - s;
   let res = ((change / s) * 100).toFixed(2);
   profitLoss.textContent = `${res} %`;
+    
+  } catch (err) {
+    alert(err);
+  }
+
 }
+
+closeBtn.addEventListener("click", () => {
+  modal.classList.remove("show");
+});
